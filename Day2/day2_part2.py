03a2fa3242d9a7d3afe171918b39dd0d19277f81
SKendrish <stephenkendrish@gmail.com>
2024-12-06 16:00:48 -0600

    Day2

--- Day2/day2_part2.py
+++ Day2/day2_part2.py
@@ -14,24 +14,34 @@ def conv_to_numlist(x):
 reports = f.readlines()
 
 numlist = conv_to_numlist(reports)
+badReports = []
+def isSafe(list):
+    inc = 0
+    dec = 0
+    lenList = len(list)
+    safe = False
+    for i in range(0, lenList - 1):
+        first = list[i]
+        second = list[i+1]
+        lenList = len(list)
+        #increasing
+        if first < second and abs(first-second) <= 3 and first != second:
+            inc += 1
+        #decreasing
+        if first > second and abs(first-second) <= 3 and first != second:
+            dec += 1
+        if inc == lenList - 1 or dec == lenList - 1:
+            safe = True
+    return safe
 def numSafeReports(data):
     ans = 0
     for list in data:
-        inc = 0
-        dec = 0
-        for i in range(0, len(list) - 1):
-            first = list[i]
-            second = list[i+1]
-            lenList = len(list)
-            #increasing
-            if first < second and abs(first-second) <= 3 and first != second:
-                inc += 1
-            #decreasing
-            if first > second and abs(first-second) <= 3 and first != second:
-                dec += 1
-            if inc == lenList - 1 or dec == lenList - 1:
-                ans += 1
+        if isSafe(list):
+            ans += 1
+        if not isSafe(list):
+            
     return ans
 
 print(numSafeReports(numlist))
+
 f.close()
